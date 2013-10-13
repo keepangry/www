8039ae3c0adb4fc207a488b2a5c29d9966d3c9a2
yangsen <yangsencs@gmail.com>
2013-10-13 17:40:23 +0400

    初始化www thinkphp

---
+++ Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+
+	#'THINK_PLUGIN_ON'=>true,
+	//使用smarty引擎
+	'TMPL_ENGINE_TYPE'=>'Smarty',
+	//'TMPL_TEMPLATE_SUFFIX'=>'.html',
+	'TMPL_ENGINE_CONFIG'=>array(
+		'left_delimiter'=>'<{',
+		'right_delimiter'=>'}>',
+	),
+);
+?>
---
+++ webroot/index.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_DEBUT',TRUE);
+
+define('APP_NAME','home');
+define('APP_PATH','../');
+
+require '../ThinkPHP/ThinkPHP.php';
